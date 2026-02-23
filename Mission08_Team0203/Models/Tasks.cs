856391ccbee65970679f3f79acd649cda9c127b9
Rockylane01 <rockylane01@gmail.com>
2026-02-23 09:39:29 -0700

    Add Model

---
+++ Mission08_Team0203/Models/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0203.Models
+{
+    public class Tasks
+    {
+        [Required]
+        public string Task { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        [Range(1, 4, ErrorMessage = "Quadrant must be between 1 and 4.")]
+        public int Quadrant { get; set; }
+
+        public string? Category { get; set; }
+
+        [Required]
+        public bool Completed { get; set; }
+    }
+}
--- Mission08_Team0203/obj/Debug/net10.0/Mission08_Team0203.AssemblyInfo.cs
+++ Mission08_Team0203/obj/Debug/net10.0/Mission08_Team0203.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f8f377a3f189541816ded3f3d956fa92a5ec0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b654ac71f1bd494e123232c22ebcdccc543b4dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
