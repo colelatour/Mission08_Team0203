378854b56b2ca30bec5a26df6897a46925c1167c
colelatour <clatour0@byu.edu>
2026-02-26 08:28:01 -0700

    Test v3

--- Mission08_Team0203/obj/Debug/net10.0/Mission08_Team0203.AssemblyInfo.cs
+++ Mission08_Team0203/obj/Debug/net10.0/Mission08_Team0203.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2750c2081ccd46b74417eece7a4832a8245f4d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2de6f4e3b3e655a16d1beb770446232bda5db3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission08_Team0203")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
